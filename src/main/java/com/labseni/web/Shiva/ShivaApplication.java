208c2ddc3d2660945b8e6d30faf21332f4c3c5e0
Aziz Amerul Faozi <azizfaozi@prasimax.com>
2019-05-08 14:45:09 +0700

    Perdana

---
+++ src/main/java/com/labseni/web/Shiva/ShivaApplication.java
@@ -0,0 +1,13 @@
+package com.labseni.web.Shiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivaApplication.class, args);
+	}
+
+}
